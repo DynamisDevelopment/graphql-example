0ee1a59ec9d34e7375e5ddb1fec4518cf4f1ae1c
unknown <devlead@dynamisdevelopment.com>
2019-07-14 16:13:23 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+
+const users = [
+    {
+        name: 'Chomp',
+        age: 47,
+        friends: ['Alli', 'Jaws']
+    }, {
+        name: 'Alli',
+        age: 62,
+        friends: ['Chomp', 'Jaws']
+    }, {
+        name: 'Jaws',
+        age: 35,
+        friends: ['Alli', 'Chomp']
+    },
+]
+
+const typeDefs = `
+ type Query {
+    user(name: String!): [User!]!
+  }
+
+  type User {
+    name: String!
+    age: Int!
+    friends: [User!]
+  }
+`
+
+const resolvers = {
+    Query: {
+        user(parent, args, ctx, info) {
+            if (!args.name) return users
+            else return users.filter(user => {
+                return user.name.toLowerCase().includes(args.name.toLowerCase())
+            })
+        }
+    },
+    User: {
+        friends(parent, args, ctx, info) {
+            const friends = []
+
+            parent.friends.forEach(friend => users.filter(user => {
+                if (user.name.toLowerCase().includes(friend.toLowerCase())) friends.push(user)
+            }))
+
+            return friends
+        }
+    }
+
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+
+server.start(() => console.log('server running'))
